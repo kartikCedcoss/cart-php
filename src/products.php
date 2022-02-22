56c9a46c743e450a2440ee73459b5d2609224345
Kartik saini <kartiksaini@cedcommerce.com>
2022-02-22 19:02:03 +0530

    temp ommit

---
+++ src/products.php
@@ -0,0 +1,49 @@
+<?php include 'header.php' ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Products
+	</title>
+	<link href="style.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+	
+	<div id="main">
+		<div id="products">
+			<div id="product-101" class="product">
+				<img src="images/football.png">
+				<h3 class="title"><a href="#">Product 101</a></h3>
+				<span>Price: $150.00</span>
+				<a class="add-to-cart" href="#">Add To Cart</a>
+			</div>
+			<div id="product-101" class="product">
+				<img src="images/tennis.png">
+				<h3 class="title"><a href="#">Product 102</a></h3>
+				<span>Price: $120.00</span>
+				<a class="add-to-cart" href="#">Add To Cart</a>
+			</div>
+			<div id="product-101" class="product">
+				<img src="images/basketball.png">
+				<h3 class="title"><a href="#">Product 103</a></h3>
+				<span>Price: $90.00</span>
+				<a class="add-to-cart" href="#">Add To Cart</a>
+			</div>
+			<div id="product-101" class="product">
+				<img src="images/table-tennis.png">
+				<h3 class="title"><a href="#">Product 104</a></h3>
+				<span>Price: $110.00</span>
+				<a class="add-to-cart" href="#">Add To Cart</a>
+			</div>
+			<div id="product-101" class="product">
+				<img src="images/soccer.png">
+				<h3 class="title"><a href="#">Product 105</a></h3>
+				<span>Price: $80.00</span>
+				<a class="add-to-cart" href="#">Add To Cart</a>
+			</div>
+		</div>
+	</div>
+	<div> 
+		
+	</div>
+<?php include 'footer.php'?>
